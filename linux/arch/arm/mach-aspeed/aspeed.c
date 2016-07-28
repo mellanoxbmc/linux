b51b6c8f3010003d7fa46283086d0f493cfe7867
Shay Slobodkin <shays@mellanox.com>
2016-07-28 15:50:50 +0300

    Merged remote branch 4.6. Latest commit from origin:
    ----
    	ommit 77b7fb556c56542e33cfe3acd22489c2199841bd
    	uthor: Joel Stanley <joel@jms.id.au>
    	ate:   Tue Jul 26 13:36:31 2016 +0930
    
    	   arm/dts/aspeed: Move all boards to 115200
    
    	   Our u-boot now uses 115200, so update all of the chosen nodes to use
    	   this too.
    
    	   Signed-off-by: Joel Stanley <joel@jms.id.au>
    
    ----
    
    Signed-off-by: Shay Slobodkin <shays@mellanox.com>

---
+++ linux/arch/arm/mach-aspeed/aspeed.c
@@ -0,0 +1,262 @@
+#include <linux/init.h>
+#include <linux/of_irq.h>
+#include <linux/of_platform.h>
+#include <linux/io.h>
+#include <linux/pinctrl/machine.h>
+#include <linux/pinctrl/consumer.h>
+#include <asm/mach-types.h>
+#include <asm/mach/arch.h>
+#include <asm/mach/map.h>
+
+// XXX TEMP HACKERY
+//
+// To be replaced by proper clock, pinmux and syscon drivers operating
+// from DT parameters
+static struct pinctrl_map palmetto_mapping[] __initdata = {
+	PIN_MAP_MUX_GROUP_DEFAULT("i2c-3", "1e6e2000.pinmux", NULL, "I2C3"),
+	PIN_MAP_MUX_GROUP_DEFAULT("i2c-4", "1e6e2000.pinmux", NULL, "I2C4"),
+	PIN_MAP_MUX_GROUP_DEFAULT("i2c-5", "1e6e2000.pinmux", NULL, "I2C5"),
+	PIN_MAP_MUX_GROUP_DEFAULT("i2c-6", "1e6e2000.pinmux", NULL, "I2C6"),
+	PIN_MAP_MUX_GROUP_DEFAULT("i2c-7", "1e6e2000.pinmux", NULL, "I2C7"),
+	PIN_MAP_MUX_GROUP_DEFAULT("i2c-8", "1e6e2000.pinmux", NULL, "I2C8"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "NCTS4"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "NDCD4"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "NRI4"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "TXD4"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "RXD4"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "FLBUSY"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "FLWP"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "VGAHS"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "VGAVS"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "DDCCLK"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "DDCDAT"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "NCTS1"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "NDCD1"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "NDSR1"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "NRI1"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "NDTR1"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "NRTS1"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "TXD1"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "RXD1"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "PWM0"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "PWM1"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "PWM2"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "PWM3"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "PWM4"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "PWM5"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "PWM6"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "PWM7"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "BMCINT"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "FLACK"),
+	PIN_MAP_MUX_GROUP_HOG_DEFAULT("1e6e2000.pinmux", NULL, "ROM8"),
+};
+
+static struct pinctrl_map ast2500_mapping[] __initdata = {
+	PIN_MAP_MUX_GROUP_DEFAULT("i2c-8", "1e6e2000.pinmux", NULL, "I2C9"),
+	PIN_MAP_MUX_GROUP_DEFAULT("i2c-3", "1e6e2000.pinmux", NULL, "I2C4"),
+	PIN_MAP_MUX_GROUP_DEFAULT("i2c-2", "1e6e2000.pinmux", NULL, "I2C3"),
+};
+
+static void __init aspeed_dt_init(void)
+{
+	int ret = 0;
+
+	if (of_machine_is_compatible("tyan,palmetto-bmc"))
+		ret = pinctrl_register_mappings(palmetto_mapping,
+				ARRAY_SIZE(palmetto_mapping));
+
+	if (of_machine_is_compatible("aspeed,ast2500-evb"))
+		ret = pinctrl_register_mappings(ast2500_mapping,
+				ARRAY_SIZE(ast2500_mapping));
+	if (ret)
+		printk("Failed to register mappings with pinmux :(\n");
+
+	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
+}
+
+#define AST_IO_VA	0xf0000000
+#define AST_IO_PA	0x1e600000
+#define AST_IO_SZ	0x00200000
+
+#define AST_IO(__pa)	((void __iomem *)(((__pa) & 0x001fffff) | AST_IO_VA))
+
+#define AST_BASE_LPC		0x1E789000 /* LPC Controller */
+#define AST_BASE_SPI		0x1E630000 /* SPI Memory Controller */
+#define AST_BASE_MAC0		0X1E660000 /* MAC 1 */
+#define AST_BASE_SCU		0x1E6E2000 /* System Control Unit (SCU) */
+#define AST_BASE_GPIO		0x1E780000 /* GPIO Controller */
+
+static struct map_desc aspeed_io_desc[] __initdata __maybe_unused = {
+	{
+		.virtual	=  AST_IO_VA,
+		.pfn		= __phys_to_pfn(AST_IO_PA),
+		.length		= AST_IO_SZ,
+		.type		= MT_DEVICE
+	},
+};
+
+static void __init do_common_setup(void)
+{
+	/* Enable LPC FWH cycles, Enable LPC to AHB bridge */
+	writel(0x00000500, AST_IO(AST_BASE_LPC | 0x80));
+
+	/* Flash controller */
+	writel(0x00000003, AST_IO(AST_BASE_SPI | 0x00));
+	writel(0x00002404, AST_IO(AST_BASE_SPI | 0x04));
+
+	/* Set UART routing */
+	writel(0x00000000, AST_IO(AST_BASE_LPC | 0x9c));
+
+	/* SCU setup
+	 *  - GPIOC{4,5,6} are FUNC_MODE{0,1,2}. These nets are
+	 *    connected to the Ethernet phy and to ensure the correct
+	 *    operation they all need to be in GPIO mode (SCU90[0] = 0)
+	 *    and then pulled down
+	 */
+	writel(0x01C000FF, AST_IO(AST_BASE_SCU | 0x88));
+	writel(0xC1C000FF, AST_IO(AST_BASE_SCU | 0x8c));
+	writel(0x0000A008, AST_IO(AST_BASE_SCU | 0x90));
+
+	/* Setup scratch registers */
+	writel(0x00000042, AST_IO(AST_BASE_LPC | 0x170));
+	writel(0x00004000, AST_IO(AST_BASE_LPC | 0x174));
+}
+
+static void __init do_barreleye_setup(void)
+{
+	u32 reg;
+
+	do_common_setup();
+
+	/* Setup PNOR address mapping for 64M flash */
+	writel(0x30000C00, AST_IO(AST_BASE_LPC | 0x88));
+	writel(0xFC0003FF, AST_IO(AST_BASE_LPC | 0x8C));
+
+	/* GPIO setup */
+	writel(0x9E82FCE7, AST_IO(AST_BASE_GPIO | 0x00));
+	writel(0x0370E677, AST_IO(AST_BASE_GPIO | 0x04));
+
+	/* SCU setup
+	 *  - GPION must be set to GPIO mode (SCU88[0:7] = 0) on
+	 *    Barreleye so they can be used to read the PCIe inventory
+	 *    status
+	 */
+
+	writel(0x01C00000, AST_IO(AST_BASE_SCU | 0x88));
+
+	/*
+	 * Do read/modify/write on power gpio to prevent resetting power on
+	 * reboot
+	 */
+	reg = readl(AST_IO(AST_BASE_GPIO | 0x20));
+	reg |= 0xCFC8F7FD;
+	writel(reg, AST_IO(AST_BASE_GPIO | 0x20));
+	writel(0xC738F20A, AST_IO(AST_BASE_GPIO | 0x24));
+	writel(0x0031FFAF, AST_IO(AST_BASE_GPIO | 0x80));
+}
+
+static void __init do_palmetto_setup(void)
+{
+	do_common_setup();
+
+	/* Setup PNOR address mapping for 32M flash */
+	writel(0x30000E00, AST_IO(AST_BASE_LPC | 0x88));
+	writel(0xFE0001FF, AST_IO(AST_BASE_LPC | 0x8C));
+
+	/* GPIO setup */
+	writel(0x13008CE7, AST_IO(AST_BASE_GPIO | 0x00));
+	writel(0x0370E677, AST_IO(AST_BASE_GPIO | 0x04));
+	writel(0xDF48F7FF, AST_IO(AST_BASE_GPIO | 0x20));
+	writel(0xC738F202, AST_IO(AST_BASE_GPIO | 0x24));
+
+	/* SCU setup */
+	writel(0x01C0007F, AST_IO(AST_BASE_SCU | 0x88));
+}
+
+static void __init do_garrison_setup(void)
+{
+	do_common_setup();
+
+	/* Setup PNOR address mapping for 64M flash */
+	writel(0x30000C00, AST_IO(AST_BASE_LPC | 0x88));
+	writel(0xFC0003FF, AST_IO(AST_BASE_LPC | 0x8C));
+
+	/* SCU setup */
+	writel(0xd7000000, AST_IO(AST_BASE_SCU | 0x88));
+}
+
+static void __init do_ast2500evb_setup(void)
+{
+	unsigned long reg;
+
+	/* Reset AHB bridges */
+	writel(0x02, AST_IO(AST_BASE_SCU | 0x04));
+
+	/* Set old MDIO interface */
+	reg = readl(AST_IO(AST_BASE_MAC0 | 0x40));
+	reg &= ~0x80000000;
+	writel(reg, AST_IO(AST_BASE_MAC0 | 0x40));
+}
+
+#define SCU_PASSWORD	0x1688A8A8
+
+static void __init aspeed_init_early(void)
+{
+	// XXX UART stuff to fix to pinmux & co
+	writel(0x02010023, AST_IO(AST_BASE_LPC | 0x9c));
+
+	/* Unlock SCU */
+	writel(SCU_PASSWORD, AST_IO(AST_BASE_SCU));
+
+	/* Enable UART4 RXD4, TXD4, NRI4, NDCD4, NCTS4 */
+	writel(0xcb000000, AST_IO(AST_BASE_SCU | 0x80));
+
+	/* Enable
+	 *  - UART1 RXD1, RXD1, NRTS1, NDTR1, NRI1, NDSR1, NDCD1, NCTS1.
+	 *  - VGA DDCDAT, DDCCLK, VGAVS, VGAHS.
+	 *  - NAND flash FLWP#, FLBUSY#
+	 */
+	writel(0x00fff0c0, AST_IO(AST_BASE_SCU | 0x84));
+
+	/* Enables all the clocks except D2CLK, USB1.1 Host, USB1.1, LHCLK */
+	writel(0x10CC5E80, AST_IO(AST_BASE_SCU | 0x0c));
+
+	/*
+	 * Ensure all IPs except GPIO and LPC are reset on watchdog expiry
+	 */
+	writel(0x001fdff3, AST_IO(AST_BASE_SCU | 0x9c));
+
+	/*
+	 * Temporary setup of AST registers until pinmux driver is complete
+	 */
+	if (of_machine_is_compatible("rackspace,barreleye-bmc"))
+		do_barreleye_setup();
+	if (of_machine_is_compatible("tyan,palmetto-bmc"))
+		do_palmetto_setup();
+	if (of_machine_is_compatible("ibm,garrison-bmc"))
+		do_garrison_setup();
+	if (of_machine_is_compatible("aspeed,ast2500-evb"))
+		do_ast2500evb_setup();
+
+}
+
+static void __init aspeed_map_io(void)
+{
+	iotable_init(aspeed_io_desc, ARRAY_SIZE(aspeed_io_desc));
+	debug_ll_io_init();
+
+	printk("SOC Rev: %08x\n", readl(AST_IO(AST_BASE_SCU | 0x7c)));
+}
+
+static const char *const aspeed_dt_match[] __initconst = {
+		"aspeed,ast2400",
+		"aspeed,ast2500",
+		NULL,
+};
+
+DT_MACHINE_START(aspeed_dt, "ASpeed SoC")
+	.init_early	= aspeed_init_early,
+	.dt_compat	= aspeed_dt_match,
+	.map_io		= aspeed_map_io,
+	.init_machine	= aspeed_dt_init,
+MACHINE_END
---
+++ linux/drivers/pinctrl/pinctrl-ast2500.c
@@ -0,0 +1,202 @@
+/*
+ * Copyright (C) 2016 IBM Corp.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation version 2.
+ *
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#include <linux/bitops.h>
+#include <linux/io.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/mutex.h>
+#include <linux/of.h>
+#include <linux/platform_device.h>
+#include <linux/pinctrl/pinctrl.h>
+#include <linux/pinctrl/pinmux.h>
+#include <linux/pinctrl/pinconf.h>
+#include <linux/pinctrl/pinconf-generic.h>
+#include <linux/string.h>
+#include <linux/types.h>
+
+#include "core.h"
+#include "pinctrl-utils.h"
+#include "pinctrl-aspeed.h"
+
+#define AST2500_NR_PINS 228
+
+#define SCU80 0x80
+#define SCU90 0x90
+#define SCU94 0x94
+
+#define COND1 FUNC_DESC_BIT(SCU90, 6, 0)
+#define COND2 { SCU94, GENMASK(1, 0), 0, 0 }
+
+FUNC_EXPR_DECL_SINGLE(I2C9, FUNC_DESC_SET(SCU90, 22), COND1);
+
+#define C14 4
+FUNC_EXPR_DECL_SINGLE(TIMER5, FUNC_DESC_SET(SCU80, 4), COND1);
+MS_PIN_DECL(C14, GPIOA4, I2C9, TIMER5);
+
+FUNC_GROUP_DECL(TIMER5, C14);
+
+#define A13 5
+FUNC_EXPR_DECL_SINGLE(TIMER6, FUNC_DESC_SET(SCU80, 5), COND1);
+MS_PIN_DECL(A13, GPIOA4, I2C9, TIMER6);
+
+FUNC_GROUP_DECL(TIMER6, A13);
+
+FUNC_GROUP_DECL(I2C9, C14, A13);
+
+FUNC_EXPR_DECL_SINGLE(I2C4, FUNC_DESC_SET(SCU90, 17));
+
+#define A9 130
+SS_PIN_DECL(A9, GPIOQ2, I2C4);
+
+#define B9 131
+SS_PIN_DECL(B9, GPIOQ3, I2C4);
+
+FUNC_GROUP_DECL(I2C4, A9, B9);
+
+/* I2C3 */
+
+FUNC_EXPR_DECL_SINGLE(I2C3, FUNC_DESC_SET(SCU90, 16), COND1);
+
+#define A11 128
+SS_PIN_DECL(A11, GPIOQ0, I2C3);
+
+#define A10 129
+SS_PIN_DECL(A10, GPIOQ1, I2C3);
+
+FUNC_GROUP_DECL(I2C3, A11, A10);
+
+
+static struct pinctrl_pin_desc ast2500_pins[AST2500_NR_PINS] = {
+	ASPEED_PINCTRL_PIN(C14),
+	ASPEED_PINCTRL_PIN(A13),
+	ASPEED_PINCTRL_PIN(A11),
+	ASPEED_PINCTRL_PIN(A10),
+	ASPEED_PINCTRL_PIN(A9),
+	ASPEED_PINCTRL_PIN(B9),
+};
+
+static const struct aspeed_pin_group ast2500_groups[] = {
+	ASPEED_PINCTRL_GROUP(I2C9),
+	ASPEED_PINCTRL_GROUP(I2C4),
+	ASPEED_PINCTRL_GROUP(I2C3),
+};
+
+static const struct aspeed_pin_function ast2500_functions[] = {
+	ASPEED_PINCTRL_FUNC(I2C9),
+	ASPEED_PINCTRL_FUNC(I2C4),
+	ASPEED_PINCTRL_FUNC(I2C3),
+};
+
+static struct aspeed_pinctrl_data ast2500_pinctrl = {
+	.pins = ast2500_pins,
+	.npins = ARRAY_SIZE(ast2500_pins),
+	.groups = ast2500_groups,
+	.ngroups = ARRAY_SIZE(ast2500_groups),
+	.functions = ast2500_functions,
+	.nfunctions = ARRAY_SIZE(ast2500_functions),
+};
+
+static struct pinmux_ops ast2500_pinmux_ops = {
+	.get_functions_count = aspeed_pinmux_get_fn_count,
+	.get_function_name = aspeed_pinmux_get_fn_name,
+	.get_function_groups = aspeed_pinmux_get_fn_groups,
+	.set_mux = aspeed_pinmux_set_mux,
+	.gpio_request_enable = aspeed_gpio_request_enable,
+	.strict = true,
+};
+
+static struct pinctrl_ops ast2500_pinctrl_ops = {
+	.get_groups_count = aspeed_pinctrl_get_groups_count,
+	.get_group_name = aspeed_pinctrl_get_group_name,
+	.get_group_pins = aspeed_pinctrl_get_group_pins,
+	.pin_dbg_show = aspeed_pinctrl_pin_dbg_show,
+	.dt_node_to_map = pinconf_generic_dt_node_to_map_pin,
+	.dt_free_map = pinctrl_utils_dt_free_map,
+};
+
+static struct pinconf_ops ast2500_pinconf_ops = {
+	.pin_config_get = aspeed_pin_config_get,
+	.pin_config_set = aspeed_pin_config_set,
+};
+
+static struct pinctrl_desc ast2500_pinctrl_desc = {
+	.name = "pinctrl-ast2500",
+	.pins = ast2500_pins,
+	.npins = ARRAY_SIZE(ast2500_pins),
+	.pctlops = &ast2500_pinctrl_ops,
+	.pmxops = &ast2500_pinmux_ops,
+	.confops = &ast2500_pinconf_ops,
+	.owner = THIS_MODULE,
+};
+
+static struct pinctrl_gpio_range ast2500_gpios = {
+	.name = "ast2500-pctrl-gpio-range",
+	/* HACK: this is even auto-assigned by the GPIO subsystem, so there are
+	 * no guarantees. BUT. It seems to pick 320 consistently.
+	 */
+	.base = 320,
+	.npins = ARRAY_SIZE(ast2500_pins),
+};
+
+static int __init ast2500_pinctrl_probe(struct platform_device *pdev)
+{
+	struct aspeed_pinctrl_data *pdata = &ast2500_pinctrl;
+	struct resource *res;
+	struct pinctrl_dev *pctl;
+	int i;
+
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	pdata->reg_base = devm_ioremap_resource(&pdev->dev, res);
+	if (IS_ERR(pdata->reg_base))
+		return PTR_ERR(pdata->reg_base);
+
+	/* We allocated space for all pins, make sure we initialise all pin
+	 * numbers. Those pins we haven't defined won't yet have had their
+	 * number initialised, and it's effectively a no-op for those which
+	 * have.
+	 */
+	for (i = 0; i < ARRAY_SIZE(ast2500_pins); i++)
+		ast2500_pins[i].number = i;
+
+	pctl = pinctrl_register(&ast2500_pinctrl_desc, &pdev->dev, pdata);
+
+	if (IS_ERR(pctl)) {
+		dev_err(&pdev->dev, "Failed to register pinctrl\n");
+		return PTR_ERR(pctl);
+	}
+
+	platform_set_drvdata(pdev, pdata);
+
+	/* grange.name = "exynos5440-pctrl-gpio-range"; */
+	pinctrl_add_gpio_range(pctl, &ast2500_gpios);
+
+	return 0;
+}
+
+static const struct of_device_id ast2500_pinctrl_of_match[] = {
+	{ .compatible = "aspeed,ast2500-pinctrl", },
+	{ },
+};
+
+static struct platform_driver ast2500_pinctrl_driver = {
+	.driver = {
+		.name = "pinctrl-ast2500",
+		.of_match_table = ast2500_pinctrl_of_match,
+	},
+};
+
+module_platform_driver_probe(ast2500_pinctrl_driver, ast2500_pinctrl_probe);
+
+MODULE_AUTHOR("Andrew Jeffery <andrew@aj.id.au>");
+MODULE_DESCRIPTION("ASPEED AST2500 pinctrl driver");
+MODULE_LICENSE("GPL v2");
